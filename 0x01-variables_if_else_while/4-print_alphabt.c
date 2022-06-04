dcc6b69eb5607b6026cca2eec344d99c05adc425
dushimire20 <odushimire922@daviscollege.com>
2022-06-04 05:08:18 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+*main - print all alphabet in lower case except q and e
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a != q && a != e)
+putchar(a);
+
+}
+putchar('\n');
+return (0);
+}
