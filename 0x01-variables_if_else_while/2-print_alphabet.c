44f08a437107884886915f07bb144f1d0d81f58b
dushimire20 <odushimire922@daviscollege.com>
2022-06-04 04:41:51 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+*main - Display alphabet in lowercase
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+putchar('\n')
+}
+return (0);
+}
