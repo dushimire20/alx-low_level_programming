dc91c3e3494b78f458eb8f93d2e20f1220cf3114
dushimire20 <odushimire922@daviscollege.com>
2022-06-04 03:55:56 -0700

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+*main - assin a random number to a variable
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int ld;
+ld = n % 10;
+if (ld > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+}
+else if (ld == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, ld);
+}
+else if (ld < 6 && ld != 0)
+{
+printf("Last digit of % is % and is less than 6 and is not 0\n", n, ld);
+}
+return (0);
+}
