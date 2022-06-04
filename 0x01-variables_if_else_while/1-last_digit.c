2ac25fd30eaa3d43cad3582806f57d79c29e7350
dushimire20 <odushimire922@daviscollege.com>
2022-06-04 04:12:35 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is 0\n", n, ld);
 }
 else if (ld < 6 && ld != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, ld);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 }
 return (0);
 }
