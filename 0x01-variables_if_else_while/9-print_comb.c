fa141f196c0bd49ab7fc974c555ce0d34fb5c508
dushimire20 <odushimire922@daviscollege.com>
2022-06-06 04:07:16 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+*main - number separated by commas and space
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+if (n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
