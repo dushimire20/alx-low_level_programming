58d9e7604cc6f55acafaf063b5f9b36412776871
dushimire20 <odushimire922@daviscollege.com>
2022-06-07 10:58:23 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+*print_alphabet_x10 - a function that print 10 times the alphabet
+*
+*Return: Always 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+int a,n;
+n = 1;
+while (n <= 10)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+n++;
+_putchar('\n');
+}
+}
