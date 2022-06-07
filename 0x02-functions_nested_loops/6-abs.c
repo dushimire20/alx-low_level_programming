feeb2521161d50d803c8436dd96972ca03175ce6
dushimire20 <odushimire922@daviscollege.com>
2022-06-07 12:54:29 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include"main.h"
+/**
+*_abs - a function that computes the absolute value of an integer
+*@a: integer input
+*Return: Always (success)
+*/
+int _abs(int a)
+{
+return (a * ((a > 0) - (a < 0)));
+}
