e6c174e3ecadd6b471c9cfd2369544c8a64cfb9d
dushimire20 <odushimire922@daviscollege.com>
2022-06-07 13:09:05 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+*print_last_gigit - a function to print last digit
+*@ld: last digit result
+*Return: Always (success)
+*/
+int print_last_digit(int n)
+{
+int ld;
+n = (n % 10)
+
+if (n < 0)
+{
+ld = (-1 * ld);
+}
+_putchar(ld + '0');
+return (ld);
+}
