ad82812fa9a81d339a47f1bf6bc56508a76baf6a
dushimire20 <odushimire922@daviscollege.com>
2022-06-07 11:09:36 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*_islower - a function that checks for lowercase character
+*
+*Return: Always 1 or 0 (success)
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+retutn (1);
+else
+return (0);
+
+}
